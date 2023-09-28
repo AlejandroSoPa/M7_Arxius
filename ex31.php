50adad4a21e58f4ddc8eed90006ca6232d41e8bb
AlejandroSoPa <asoladopalomino.cf@iesesteveterradas.cat>
2023-09-28 19:08:41 +0200

    Ejercicio1_terminado

--- ex31.php
+++ ex31.php
@@ -2,34 +2,40 @@
 
 <?php
 
-$file = fopen("contactes.txt", "r");
+$file = fopen("contactes31.txt", "r");
 $escribir = "";
 echo "<table border=1>
+        <thead>
         <tr>
             <th>Nom</th>
             <th>Cognom1</th>
             <th>Cognom2</th>
             <th>Telefon</th>
         </tr>
+        </thead>
+        <tbody>
         "
 ;
 while(true){
-    $contenido=fgets($file);
-    
+    $contenido = fgets($file);
     if($contenido){
+        $lista = explode(",", $contenido);
+        $escribir .= "$lista[0]#$lista[1]#$lista[2]#$lista[3]";
         echo "
             <tr>
-                <td>$</td>
-                <td>$</td>
-                <td>$</td>
-                <td>$</td>
+                <td>$lista[0]</td>
+                <td>$lista[1]</td>
+                <td>$lista[2]</td>
+                <td>$lista[3]</td>
             </tr>
-        </table>
         ";
     } else {
         break;
     }
 }
 
+echo "</tbody></table>";
+$archivo = fopen("contactes31b.txt", "w");
+fwrite($archivo, $escribir);
 
 ?>
