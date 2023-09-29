a1685254f2da2709f0af02b46eed3ae81a3a6b4c
AlejandroSoPa <asoladopalomino.cf@iesesteveterradas.cat>
2023-09-29 15:55:18 +0200

    Ex32_Mejorado+Ex33_Terminado

--- ex32.php
+++ ex32.php
@@ -1,14 +1,14 @@
 <form action="" method="post">
     <p>INTRODUEIX DADES:</p>
     <textarea name="dades" id="" cols="40" rows="5"></textarea>
-    <p>Separador: <input type="text" name="sep"></p>
+    <p>Separador: <input type="text" name="separador"></p>
     <input type="submit">
 </form>
 
 <?php
 
 $file = fopen("comentaris.txt", "w");
-$file_text = preg_replace('/\s+/', $_POST["sep"], $_POST["dades"]);
-fwrite($file, $file_text);
+$texto = preg_replace('/\s+/', $_POST["separador"], $_POST["dades"]);
+fwrite($file, $texto);
 
 ?>
